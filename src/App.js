837a6ab027a5c886e1b5c415671a71d98fc30013
vasilisa1312 <Vasilisa.gorobchenko1994@mail.ru>
2023-09-06 13:46:55 +0300

    Styless added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React, { useState } from "react";
 import Counter from "./components/Counter";
 import ClassCounter from "./components/ClassCounter";
-
+import './styles/App.css';
 function App() {
   const [value, setValue] = useState("ТЕКСТ");
 
   return (
     <div className="App">
-      <ClassCounter />
+      <div className="post">
+        <div className="post__content">
+          <strong>1. JavaScrpipt</strong>
+          <div>JavaScrpipt - язык программирования</div>
+        </div>
+        <button className="post__btns">Удалить</button>
+      </div>
     </div>
   );
 }
