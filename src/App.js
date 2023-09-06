f88bdbe24d2b6da061252d6719ab3a5520d18917
vasilisa1312 <Vasilisa.gorobchenko1994@mail.ru>
2023-09-07 00:57:18 +0300

    War with button styles continue

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import "./styles/App.css";
 import PostItem from "./components/PostItem.jsx";
 import PostList from "./components/PostList";
 import MyButton from "./components/button/MyButton";
+import MyInput from "./components/input/MyInput";
 function App() {
   const [posts, setPosts] = useState([
     { id: 1, title: "JavaScript", body: "Description" },
@@ -15,11 +16,12 @@ function App() {
   return (
     <div className="App">
       <form>
-          <input type="text" placeholder="Название поста"/>
-          <input type="text" placeholder="Описание поста"/>
-          <MyButton disabled>Создать пост</MyButton>
+          <MyInput type="text" placeholder="Название поста"/>
+          <MyInput type="text" placeholder="Описание поста"/>
+          <MyButton>Создать пост</MyButton>
       </form>
       <PostList posts={posts} title={'Список постов 1'}/>
+
     </div>
   );
 }
