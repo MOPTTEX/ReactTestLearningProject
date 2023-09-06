abf6dd1b7ca9b4ed0279d98384803997cbcf4885
vasilisa1312 <Vasilisa.gorobchenko1994@mail.ru>
2023-09-06 15:10:19 +0300

    PostItem Component ADD

--- src/App.js
+++ src/App.js
@@ -2,20 +2,23 @@ import React, { useState } from "react";
 import Counter from "./components/Counter";
 import ClassCounter from "./components/ClassCounter";
 import "./styles/App.css";
-import PostItem from "./components/PostItem";
+import PostItem from "./components/PostItem.jsx";
 function App() {
-  const [posts, setPosts] = useState([
-    {id: 1, title: 'JavaScript', body: 'Description'},
-    {id: 2, title: 'JavaScript 2', body: 'Description'},
-    {id: 3, title: 'JavaScript 3', body: 'Description'},
-  ])
+  // const [posts, setPosts] = useState([
+  //   {id: 1, title: 'JavaScript', body: 'Description'},
+  //   {id: 2, title: 'JavaScript 2', body: 'Description'},
+  //   {id: 3, title: 'JavaScript 3', body: 'Description'},
+  // ])
 
   return (
     <div className="App">
-      {posts.map((post) =>
-        <PostItem post={post} key = {post.id}/>
+        <PostItem post={{id: 1, title: 'JavaScript', body: 'Description'}}/>
+        <PostItem post={{id: 2, title: 'JavaScript', body: 'Description'}}/>
+        <PostItem post={{id: 3, title: 'JavaScript', body: 'Description'}}/>
+        <PostItem post={{id: 4, title: 'JavaScript', body: 'Description'}}/>
 
-      )}
+        
+      
     </div>
   );
 }
