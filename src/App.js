f7067c770a64d2ed6abd9c81eb8bcb360fe277b4
vasilisa1312 <Vasilisa.gorobchenko1994@mail.ru>
2023-09-06 23:34:20 +0300

    button add

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import ClassCounter from "./components/ClassCounter";
 import "./styles/App.css";
 import PostItem from "./components/PostItem.jsx";
 import PostList from "./components/PostList";
+import MyButton from "./components/button/MyButton";
 function App() {
   const [posts, setPosts] = useState([
     { id: 1, title: "JavaScript", body: "Description" },
@@ -13,6 +14,11 @@ function App() {
 
   return (
     <div className="App">
+      <form>
+          <input type="text" placeholder="Название поста"/>
+          <input type="text" placeholder="Описание поста"/>
+          <MyButton disabled>Создать пост</MyButton>
+      </form>
       <PostList posts={posts} title={'Список постов 1'}/>
     </div>
   );
