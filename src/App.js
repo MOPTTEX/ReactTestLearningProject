a5cfd699542365707a540c8998c6ffd7c71f4754
vasilisa1312 <Vasilisa.gorobchenko1994@mail.ru>
2023-09-06 15:42:06 +0300

    PostList ADD

--- src/App.js
+++ src/App.js
@@ -3,22 +3,17 @@ import Counter from "./components/Counter";
 import ClassCounter from "./components/ClassCounter";
 import "./styles/App.css";
 import PostItem from "./components/PostItem.jsx";
+import PostList from "./components/PostList";
 function App() {
-  // const [posts, setPosts] = useState([
-  //   {id: 1, title: 'JavaScript', body: 'Description'},
-  //   {id: 2, title: 'JavaScript 2', body: 'Description'},
-  //   {id: 3, title: 'JavaScript 3', body: 'Description'},
-  // ])
+  const [posts, setPosts] = useState([
+    { id: 1, title: "JavaScript", body: "Description" },
+    { id: 2, title: "JavaScript 2", body: "Description" },
+    { id: 3, title: "JavaScript 3", body: "Description" },
+  ]);
 
   return (
     <div className="App">
-        <PostItem post={{id: 1, title: 'JavaScript', body: 'Description'}}/>
-        <PostItem post={{id: 2, title: 'JavaScript', body: 'Description'}}/>
-        <PostItem post={{id: 3, title: 'JavaScript', body: 'Description'}}/>
-        <PostItem post={{id: 4, title: 'JavaScript', body: 'Description'}}/>
-
-        
-      123
+      <PostList posts={posts} title={'Список постов 1'}/>
     </div>
   );
 }
