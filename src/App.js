d4f204d6083d19b4a5c8e56588ea531af8fe83a1
vasilisa1312 <Vasilisa.gorobchenko1994@mail.ru>
2023-09-07 20:57:40 +0300

    SORT posts added

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import PostForm from "./components/PostForm";
 import MySelect from "./components/select/MySelect";
 function App() {
   const [posts, setPosts] = useState([
-    { id: 1, title: "JavaScript", body: "Description" },
-    { id: 2, title: "JavaScript 2", body: "Description" },
-    { id: 3, title: "JavaScript 3", body: "Description" },
+    { id: 1, title: "aa", body: "Deswon" },
+    { id: 2, title: "Jccc", body: "Descrition" },
+    { id: 3, title: "Jweraipt 3", body: "Description" },
   ]);
+
+const [selectedSort, setSelectedSort] = useState('')
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
@@ -18,12 +20,19 @@ function App() {
     setPosts(posts.filter((p) => p.id !== post.id));
   };
 
+const sortPosts = (sort) => {
+  setSelectedSort(sort);
+  setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])))
+}
+
   return (
     <div className="App">
       <PostForm create={createPost} />
       <hr style={{ margin: "15px 0" }} />
       <div>
         <MySelect 
+          value={selectedSort}
+          onChange={sortPosts}
           defaultValue='Сортировка'
           options={[
             {value: 'title', name: 'По названию'},
