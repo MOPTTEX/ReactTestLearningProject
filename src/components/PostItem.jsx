016789f65e319808af3b0cbbf46d70394bf10e06
vasilisa1312 <Vasilisa.gorobchenko1994@mail.ru>
2023-09-07 20:03:52 +0300

    Delete Post button ADD

--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import MyButton from './button/MyButton'
 
 export default function PostItem(props) {
   return (
@@ -9,7 +10,7 @@ export default function PostItem(props) {
                 <div>{props.post.body}</div>
             </div>
             <div className="post__btns">
-                <button>Удалить</button>
+                <MyButton onClick = {() => props.remove(props.post)}>Удалить</MyButton>
             </div>
         </div>
     </div>
