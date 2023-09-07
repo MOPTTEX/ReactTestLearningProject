3d6a860d1c41ea87682f0d18713e574f001779b0
vasilisa1312 <Vasilisa.gorobchenko1994@mail.ru>
2023-09-07 19:56:29 +0300

    postForm added

---
+++ src/components/PostForm.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import MyInput from './input/MyInput';
+import MyButton from './button/MyButton';
+
+export default function PostForm({ create }) {
+  const addNewPost = (e) => {
+    e.preventDefault();
+    const newPost = {
+      ...post,
+      id: Date.now(),
+    };
+    create(newPost);
+    setPost({ title: "", body: "" });
+  };
+
+  const [post, setPost] = useState({ title: "", body: "" });
+  return (
+    <form>
+      <MyInput
+        type="text"
+        placeholder="Название поста"
+        value={post.title}
+        onChange={(e) => setPost({ ...post, title: e.target.value })}
+      />
+      <MyInput
+        value={post.body}
+        onChange={(e) => setPost({ ...post, body: e.target.value })}
+        type="text"
+        placeholder="Описание поста"
+      />
+      <MyButton onClick={addNewPost}>Создать пост</MyButton>
+    </form>
+  );
+}
