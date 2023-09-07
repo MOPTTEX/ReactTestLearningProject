430268d9eb848a1c34da734e094baf4d32b02a5d
vasilisa1312 <Vasilisa.gorobchenko1994@mail.ru>
2023-09-07 21:17:47 +0300

    Find label add

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React, {useState } from "react";
+import React, { useState } from "react";
 import "./styles/App.css";
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
 import MySelect from "./components/select/MySelect";
+import MyInput from "./components/input/MyInput";
 function App() {
   const [posts, setPosts] = useState([
     { id: 1, title: "aa", body: "Deswon" },
@@ -10,7 +11,18 @@ function App() {
     { id: 3, title: "Jweraipt 3", body: "Description" },
   ]);
 
-const [selectedSort, setSelectedSort] = useState('')
+  const [selectedSort, setSelectedSort] = useState("");
+  const [searchQuery, setSearchQuery] = useState("");
+
+  function getSortedPosts() {
+    console.log("отработала функция сортед постс")
+    if(selectedSort) {
+      return [...posts].sort((a, b) => a[selectedSort].localeCompare(b[selectedSort]))
+    }
+    return posts;
+  }
+
+  const sortedPosts = getSortedPosts();
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
@@ -20,28 +32,32 @@ const [selectedSort, setSelectedSort] = useState('')
     setPosts(posts.filter((p) => p.id !== post.id));
   };
 
-const sortPosts = (sort) => {
-  setSelectedSort(sort);
-  setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])))
-}
+  const sortPosts = (sort) => {
+    setSelectedSort(sort);
+  };
 
   return (
     <div className="App">
       <PostForm create={createPost} />
       <hr style={{ margin: "15px 0" }} />
       <div>
-        <MySelect 
+        <MyInput
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          placeholder="Поиск"
+        />
+        <MySelect
           value={selectedSort}
           onChange={sortPosts}
-          defaultValue='Сортировка'
+          defaultValue="Сортировка"
           options={[
-            {value: 'title', name: 'По названию'},
-            {value: 'body', name: 'По описанию'},
+            { value: "title", name: "По названию" },
+            { value: "body", name: "По описанию" },
           ]}
         />
       </div>
       {posts.length ? (
-        <PostList remove={removePost} posts={posts} title={"Список постов "} />
+        <PostList remove={removePost} posts={sortedPosts} title={"Список постов "} />
       ) : (
         <h1 style={{ textAlign: "center" }}> Посты не найдены!</h1>
       )}
