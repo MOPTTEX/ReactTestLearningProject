b58dcc4391ff786ed1b4ff174891f8c0d0c74361
vasilisa1312 <Vasilisa.gorobchenko1994@mail.ru>
2023-09-07 15:31:35 +0300

    Post add ready

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import Counter from "./components/Counter";
 import ClassCounter from "./components/ClassCounter";
 import "./styles/App.css";
@@ -13,14 +13,33 @@ function App() {
     { id: 3, title: "JavaScript 3", body: "Description" },
   ]);
 
+  const [post, setPost] = useState({title: '', body: ''});
+
+  const addNewPost = (e) => {
+    e.preventDefault();
+
+    setPosts([...posts, {...post, id: Date.now()}])
+    setPost({title: '', body: ''})
+  };
+
   return (
     <div className="App">
       <form>
-          <MyInput type="text" placeholder="Название поста"/>
-          <MyInput type="text" placeholder="Описание поста"/>
-          <MyButton>Создать пост</MyButton>
+        <MyInput
+          type="text"
+          placeholder="Название поста"
+          value={post.title}
+          onChange={(e) => setPost({...post, title: e.target.value})}
+        />
+        <MyInput
+          value={post.body}
+          onChange={(e) => setPost({...post, body: e.target.value})}
+          type="text"
+          placeholder="Описание поста"
+        />
+        <MyButton onClick={addNewPost}>Создать пост</MyButton>
       </form>
-      <PostList posts={posts} title={'Список постов 1'}/>
+      <PostList posts={posts} title={"Список постов "} />
     </div>
   );
 }
--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -5,7 +5,7 @@ export default function PostItem(props) {
     <div>
         <div className="post">
             <div className="post__content">
-                <strong>{props.post.id}. {props.post.title}</strong>
+                <strong>{props.number}. {props.post.title}</strong>
                 <div>{props.post.body}</div>
             </div>
             <div className="post__btns">
