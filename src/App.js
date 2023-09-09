d33e13718f841225516912ec313992780c08f21c
vasilisa1312 <Vasilisa.gorobchenko1994@mail.ru>
2023-09-09 21:01:35 +0300

    Axios and getPost button add

--- src/App.js
+++ src/App.js
@@ -6,8 +6,9 @@ import PostFilter from "./components/PostFilter";
 import MyModal from "./components/MyModal/MyModal";
 import MyButton from "./components/button/MyButton";
 import { usePosts,} from "./hooks/usePosts";
+import axios from "axios";
 function App() {
-  const [posts, setPosts] = useState();
+  const [posts, setPosts] = useState([]);
 
   const [filter, setFilter] = useState({ sort: "", query: "" });
   const [modal, setModal] = useState(false);
@@ -18,12 +19,18 @@ function App() {
     setModal(false);
   };
 
+ async function fetchPosts() {
+    const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
+    setPosts(response.data)
+  }
+
   const removePost = (post) => {
     setPosts(posts.filter((p) => p.id !== post.id));
   };
 
   return (
     <div className="App">
+      <button onClick={fetchPosts}>GET POSTS</button>
       <MyButton style={{ marginTop: 30 }} onClick={() => setModal(true)}>
         Создать пользователя
       </MyButton>
