afebe18b787c61849e788b88a918c1acc4860527
vasilisa1312 <Vasilisa.gorobchenko1994@mail.ru>
2023-09-09 20:08:28 +0300

    Transition v 2.0 finished

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -12,7 +12,7 @@ export default function PostList({ posts, title, remove }) {
       <h1 style={{ textAlign: "center" }}>{title}</h1>
       <TransitionGroup>
         {posts.map((post, index) => (
-          <CSSTransition key={post.id} timeout={500} classNames="item">
+          <CSSTransition key={post.id} timeout={500} classNames="post">
             <PostItem remove={remove} number={index + 1} post={post} />
           </CSSTransition>
         ))}
