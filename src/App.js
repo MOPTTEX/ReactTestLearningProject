8401f02cc6442d6397c9a44da50b506378c209ef
vasilisa1312 <Vasilisa.gorobchenko1994@mail.ru>
2023-09-12 20:40:39 +0300

    AppRouter component add

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
 import React, {  } from "react";
 import "./styles/App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import About from "./pages/About";
-import Posts from "./pages/Posts";
+import { BrowserRouter } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
-import Error from './pages/Error';
+import AppRouters from "./components/AppRouters";
 function App() {
  return (
   <BrowserRouter>
   <Navbar/>
+  <AppRouters/>
 
-  <Routes>
-    <Route path="/about" element={<About/>}/>
-    <Route path="/posts" element={<Posts/>}/>
-    <Route path="*" element={<Error/>} />
-  </Routes>
   </BrowserRouter>
  )
 }
