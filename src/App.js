2aef3905f4f963f6a9c15428f6589e4942bfc338
vasilisa1312 <Vasilisa.gorobchenko1994@mail.ru>
2023-09-12 20:22:49 +0300

    Links and navbar add

--- src/App.js
+++ src/App.js
@@ -6,6 +6,12 @@ import Posts from "./pages/Posts";
 function App() {
  return (
   <BrowserRouter>
+  <div className="navbar">
+    <div className="navbar__links">
+      <a href="/about"> О нас</a>
+      <a href="/posts"> Посты </a>
+    </div>
+  </div>
   <Routes>
     <Route path="/about" element={<About/>}/>
     <Route path="/posts" element={<Posts/>}/>
