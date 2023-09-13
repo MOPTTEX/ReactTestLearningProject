849e7a0b05bb8f5b7de7df518f484906852bfd99
vasilisa1312 <Vasilisa.gorobchenko1994@mail.ru>
2023-09-13 18:28:35 +0300

    useParams add

--- src/pages/PostIdPage.jsx
+++ src/pages/PostIdPage.jsx
@@ -1,6 +1,8 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 export default function PostidPage() {
+  const params = useParams()
   return (
     <div>
       <h1>Вы попали на страницу поста </h1>
